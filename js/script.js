f0fe99443e01d75f5bbcb12e33a830849c9290ae
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-05 15:50:18 +0200

    Creati prompt

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+poi chiedi la sua età
+Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età) */
+
+
+/* chiedere nome tramite prompt
+chiedere cognome tramite prompt
+chiedere colore pref tramite prompt
+chiedere età tramite prompt
+stampa ritorno di ogni prompt uno di seguito all'altro */
+
+const userName = prompt("Qual è il tuo nome?");
+console.log(userName);
+// const ritornoUserName = prompt("Qual è il tuo nome?");
+// console.log(ritornoUserName);
+
+const userLastName = prompt("Qual è il tuo cognome?");
+console.log(userLastName);
+// const ritornoUserLastName = prompt("Qual è il tuo cognome?");
+// console.log(ritornoUserLastName);
+
+const favoriteColor = prompt("Qual è il tuo colore preferito?");
+console.log(favoriteColor);
+
+const age = prompt("Quanti anni hai?");
+console.log(age);
