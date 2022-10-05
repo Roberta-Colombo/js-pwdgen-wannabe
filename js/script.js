7d8a23741e9d782ca65128cc47ea45b58a3080ea
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-05 16:10:06 +0200

    aggiunto risultato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-/* Chiedi all’utente il suo nome,
+/* CONSEGNA:
+Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 poi chiedi la sua età
@@ -9,20 +10,25 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo
 chiedere cognome tramite prompt
 chiedere colore pref tramite prompt
 chiedere età tramite prompt
-stampa ritorno di ogni prompt uno di seguito all'altro */
+stampa ritorno di ogni prompt uno di seguito all'altro in un elemento html */
 
 const userName = prompt("Qual è il tuo nome?");
 console.log(userName);
-// const ritornoUserName = prompt("Qual è il tuo nome?");
-// console.log(ritornoUserName);
 
 const userLastName = prompt("Qual è il tuo cognome?");
 console.log(userLastName);
-// const ritornoUserLastName = prompt("Qual è il tuo cognome?");
-// console.log(ritornoUserLastName);
 
 const favoriteColor = prompt("Qual è il tuo colore preferito?");
 console.log(favoriteColor);
 
 const age = prompt("Quanti anni hai?");
 console.log(age);
+
+const result = `
+${userName}${userLastName}${favoriteColor}${age}
+`;
+console.log(result);
+
+const elemento = document.getElementById("riassunto");
+
+elemento.innerHTML = result; 
